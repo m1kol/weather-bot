8096fe3f5ff1c69df72f559178d2b794ae2ba1c7
Mikhail Kolesov <misha.kolesov98@gmail.com>
2020-11-18 16:49:37 +0300

    Changed API, added corresponding structures and GetForecast function. Changed bot functionality to work with new version.

--- bot/bot.go
+++ bot/bot.go
@@ -6,14 +6,24 @@ import (
 	"github.com/m1kol/weather-bot/provider"
 	"log"
 	"strings"
+	"time"
 )
 
-const replyTemplate = `Дата и время: %v
+const (
+	replyTemplate = `Дата и время: %v
 Температура: %.1f С	Максимальная: %.1f С Минимальная: %.1f С Ощущается как: %.1f С
 Погода: %v
 Скорость ветра: %.1f м/с
 
 `
+	forecastTemplate = `Дата и время: %v
+Температура: утром %.f С днём  %.f С вечером  %.f С ночью %.f С
+Ощущается как: утром %.f С днём  %.f С вечером  %.f С ночью %.f С
+Погода: %v
+Скорость ветра: %.1f м/с
+
+`
+)
 
 type Bot struct {
 	api *tgbotapi.BotAPI
@@ -64,14 +74,19 @@ func (bot *Bot) ProcessMessage(updates tgbotapi.UpdatesChannel) {
 		case "start":
 			msg.Text = "Hello, I'm a weather bot! I understand commands /sayhi, /status and /weather! " +
 				"/weather commands will give you a weather forecast for the day."
+
 		case "help":
 			msg.Text = "I understand /sayhi, /status and /weather."
+
 		case "sayhi":
 			msg.Text = "Hi :)"
+
 		case "status":
 			msg.Text = "I'm ok."
+
 		case "weather":
-			resp, err := bot.provider.GetWeather("Долгопрудный", 1)
+			city := update.Message.CommandArguments()
+			resp, err := bot.provider.GetWeather(city, 1)
 			if err != nil {
 				fmt.Errorf("error getting a response from weather provider: %v", err)
 			}
@@ -86,8 +101,27 @@ func (bot *Bot) ProcessMessage(updates tgbotapi.UpdatesChannel) {
 					resp.WeatherInfo[i].Weather[0].Description, resp.WeatherInfo[i].Wind.Speed,
 				)
 			}
-
 			msg.Text = builder.String()
+
+		case "forecast":
+			city := update.Message.CommandArguments()
+			forecast, err := bot.provider.GetForecast(city)
+			if err != nil {
+				log.Printf("error getting a response from weather provider: %v", err)
+			}
+
+			builder := strings.Builder{}
+			fmt.Fprintf(&builder, "Погода в городе %v\n\n", forecast.City)
+			for _, day := range forecast.Daily {
+				fmt.Fprintf(&builder, forecastTemplate,
+					time.Unix(day.Dt, 0),
+					day.Temp.Morn, day.Temp.Day, day.Temp.Eve, day.Temp.Night,
+					day.FeelsLike.Morn, day.FeelsLike.Day, day.FeelsLike.Eve, day.FeelsLike.Night,
+					day.Weather[0].Description, day.WindSpeed,
+				)
+			}
+			msg.Text = builder.String()
+
 		default:
 			msg.Text = "I don't know that command"
 		}
--- provider/provider.go
+++ provider/provider.go
@@ -10,7 +10,8 @@ import (
 
 const (
 	forecastUrl = "https://api.openweathermap.org/data/2.5/forecast"
-	//weatherUrl = "https://api.openweathermap.org/data/2.5/weather"
+	weatherUrl = "https://api.openweathermap.org/data/2.5/weather"
+	onecallUrl = "https://api.openweathermap.org/data/2.5/onecall"
 )
 
 type Provider struct {
@@ -33,16 +34,51 @@ func NewProvider(apiKey string) *Provider {
 	}
 }
 
+func (prov *Provider) GetForecast(city string) (Forecast, error) {
+	prov.config["q"] = city
+	resp, err := prov.httpClient.R().SetQueryParams(prov.config).Get(weatherUrl)
+	if err != nil {
+		return Forecast{}, fmt.Errorf("could not perform GET request: %w", err)
+	}
+	if resp.StatusCode() != http.StatusOK {
+		return Forecast{}, fmt.Errorf("unexpected status code: %v", resp.StatusCode())
+	}
+
+	var cityInfo City
+	if err := json.Unmarshal(resp.Body(), &cityInfo); err != nil {
+		return Forecast{}, fmt.Errorf("failed to unmarshall data: %w", err)
+	}
+	prov.config["lon"] = strconv.FormatFloat(cityInfo.Coord.Lon, 'f', -1, 32)
+	prov.config["lat"] = strconv.FormatFloat(cityInfo.Coord.Lat, 'f', -1, 32)
+	prov.config["exclude"] = "minutely,hourly,alerts,current"
+	delete(prov.config, "q")
+
+	resp, err = prov.httpClient.R().SetQueryParams(prov.config).Get(onecallUrl)
+	if err != nil {
+		return Forecast{}, fmt.Errorf("could not perform GET request: %w", err)
+	}
+	if resp.StatusCode() != http.StatusOK {
+		return Forecast{}, fmt.Errorf("unexpected status code: %v", resp.StatusCode())
+	}
+
+	var ret Forecast
+	if err := json.Unmarshal(resp.Body(), &ret); err != nil {
+		return Forecast{}, fmt.Errorf("failed to unmarshall data: %v", err)
+	}
+	ret.City = city
+
+	return ret, nil
+}
+
 func (prov *Provider) GetWeather(city string, days int) (Response, error) {
 	prov.config["q"] = city
 	prov.config["cnt"] = strconv.Itoa(8*days)
-
 	resp, err := prov.httpClient.R().SetQueryParams(prov.config).Get(forecastUrl)
 	if err != nil {
 		return Response{}, fmt.Errorf("could not perform GET request: %w", err)
 	}
 	if resp.StatusCode() != http.StatusOK {
-		return Response{}, fmt.Errorf("unexpected status code: %w", resp.StatusCode())
+		return Response{}, fmt.Errorf("unexpected status code: %v", resp.StatusCode())
 	}
 
 	var ret Response
@@ -51,6 +87,39 @@ func (prov *Provider) GetWeather(city string, days int) (Response, error) {
 	}
 
 	return ret, nil
+}
+
+type City struct {
+	Name  string	`json:"name"`
+	Coord struct {
+		Lon	float64 `json:"lon"`
+		Lat float64 `json:"lat"`
+	} `json:"coord"`
+}
+
+type Forecast struct {
+	City 	string
+	Daily 	[]Daily
+}
+
+type Daily struct {
+	Dt   int64 `json:"dt"`
+	Temp struct {
+		Day		float32 `json:"day"`
+		Night	float32 `json:"night"`
+		Eve		float32 `json:"eve"`
+		Morn 	float32 `json:"morn"`
+	} `json:"temp"`
+	FeelsLike struct {
+		Day		float32 `json:"day"`
+		Night	float32 `json:"night"`
+		Eve		float32 `json:"eve"`
+		Morn 	float32 `json:"morn"`
+	} `json:"feels_like"`
+	Weather []struct {
+		Description string `json:"description"`
+	} `json:"weather"`
+	WindSpeed float32 `json:"wind_speed"`
 }
 
 type Response struct {
@@ -74,5 +143,5 @@ type WeatherInfo struct {
 	Wind struct {
 		Speed float32 `json:"speed"`
 	} `json:"wind"`
-	Pop         float32 `json:"pop"`
+	Pop	float32 `json:"pop"`
 }
